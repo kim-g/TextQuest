611edb03ab8b6d7210ac20818b547ecde79168ec
Григорий Ким <kim-g@yandex.ru>
2018-06-08 18:00:02 +0500

    Начал эксперименты с GUI
    Добавил SQLite

---
+++ PhoneQuest/PhoneQuest/StartPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PhoneQuest
+{
+	public class StartPage : ContentPage
+	{
+        public StartPage() => Content = new StackLayout
+        {
+            Children = {
+                    new StackLayout
+                    {
+                        Children =
+                        {
+                            new Label
+                            {
+                                Text = "Панель вопросов",
+                                HorizontalTextAlignment = TextAlignment.Center,
+                                VerticalTextAlignment = TextAlignment.Center
+                            }
+                        }
+                    },
+
+                    new StackLayout
+                    {
+                        Children =
+                        {
+                            new Label { Text = "Панель ответов" }
+                        }
+                    }
+
+                }
+        };
+    }
+}
